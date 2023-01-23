a5d0e5d2b9569e2add4ba4712a16184a85bc07f4
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-23 17:29:01 +0200

    updated markup

--- src/index.js
+++ src/index.js
@@ -10,36 +10,58 @@ const BASE_URL = 'https://pixabay.com/api/'
 
 const formEl = document.querySelector('.search-form')
 const inputEl = document.querySelector('input')
+const galleryBoxEl = document.querySelector('.gallery')
+const loadMoreBtnEl = document.querySelector('.load-more')
+
+let page = 1;
+loadMoreBtnEl.style.display = 'none'
 
 
 
-
-// function getFetch(textInputByUser) {
-//     return fetch(`https://pixabay.com/api/?key=33055694-6965e9dfecd686cd6e0cc5baf&q=${textInputByUser}&image_type=photo&orientation=horizontal&safesearch=true&fields=downloads`)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error(response.statusText)
-//             }
-//             return response.json()
-//         })
-// }
-// getFetch('avia').then(data =>console.log(data.hits))
-// 'https://pixabay.com/api/?key=33055694-6965e9dfecd686cd6e0cc5baf&q=yellow+flowers&image_type=photo'
-
-
-function formSubmitHandler(event) {
+async function formSubmitHandler(event) {
     event.preventDefault()
+    loadMoreBtnEl.style.display = 'block'
     
-axios.get(`${BASE_URL}`,{
-    params:{key:'33055694-6965e9dfecd686cd6e0cc5baf',
-    q:inputEl.value,
+const dataAfterFetch = await axios.get(`${BASE_URL}`,{
+    params: {
+    key: '33055694-6965e9dfecd686cd6e0cc5baf',
+    q:inputEl.value.trim(),
     image_type:'photo',
     orientation:'horizontal',
     safesearch: 'true',
-    fields:'downloads'
+    fields: 'downloads',
+    page: 1,
+    per_page:40
     }
-}).then(data => console.log(data))
+}).then(data => {
+    console.log(data.data.hits)
+    const markup = data.data.hits.map(item => `<div class="photo-card">
+  <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes:${item.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views:${item.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments:${item.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads:${item.downloads}</b>
+    </p>
+  </div>
+</div>`).join('')
+
+    galleryBoxEl.innerHTML = markup
+})
+    
+   
 }
 
+// async function addMorePicturesHandler() {
+//     const getMorePictures = await axios.get()
+// }
 
-formEl.addEventListener('submit',formSubmitHandler)
+formEl.addEventListener('submit', formSubmitHandler)
+// addMoreBtnEl.addEventListener('click',addMorePicturesHandler)
